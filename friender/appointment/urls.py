6fa8fb7df4ce511e9c5035467d0c4ad2a9bcc730
yahoryakubovich <officialegoryakubovich@gmail.com>
2023-05-16 17:15:46 +0300

    Настройка navbar и user, establishment rating

--- friender/appointment/urls.py
+++ friender/appointment/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('guests/', all_guests, name='guests'),
     path('user_rating/', user_rating, name='user_rating'),
     path('establishment_rating/', establishment_rating, name='establishment_rating'),
-    re_path(r"^user_rating/((?P<id>[0-9]+)/)?$", user_form_rating, name="user_form_rating"),
-    re_path(r"^establishment_rating/((?P<id>[0-9]+)/)?$", establishment_form_rating, name="establishment_form_rating"),
+    re_path(r"^user_rating/(?P<id>[\d-]+)$", user_form_rating, name="user_form_rating"),
+    re_path(r"^establishment_rating/(?P<id>[\d-]+)$", establishment_form_rating, name="establishment_form_rating"),
     path('create_user/', create_user_form, name='create_user'),
     path('create_appointment/', create_appointment_form, name='create_appointment'),
 ]
